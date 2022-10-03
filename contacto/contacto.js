631f050b732b53eeeba1a1c0e2e020d28215c6ae
griseldaalegre <alegregriselda5@gmail.com>
2022-10-03 14:45:23 -0300

    agregro pagina de zapatillas deportivas

--- contacto/contacto.js
+++ contacto/contacto.js
@@ -6,12 +6,12 @@ form1.addEventListener('submit', validarFormulario);
 function validarFormulario(e) {
     e.preventDefault();
     let nombre = document.getElementById('nombre').value;
-    let celular = document.getElementById('celular').value;
+    let telefono = document.getElementById('telefono').value;
 
     if (nombre.length < 3) {
         resultado.innerText = "El nombre debe contener un mínimo de 3 caracteres."
-    } else if (celular.length < 10) {
-        resultado.innerText = "El celular de contacto debe contener un mínimo de 10 caracteres, incluyendo la característica local."
+    } else if (telefono.length < 10) {
+        resultado.innerText = "El telefono de contacto debe contener un mínimo de 10 caracteres, incluyendo la característica local."
     } else {
         resultado.innerText = "En breve nos pondremos en contacto. ¡Muchas gracias por elegirnos!"
     }
--- productos/zapatillas/zapatillas.js
+++ productos/zapatillas/zapatillas.js
@@ -1,23 +1,35 @@
-document.querySelectorAll(".container-img .card").forEach(el=>{
-   
-    el.addEventListener("click", function(ev){
-            ev.stopPropagation();
-            document.querySelectorAll(".container-img .card").forEach(el=>{
+document.querySelectorAll(".container-img .btn-detalles").forEach(el => {
+
+    el.addEventListener("click", function (ev) {
+        ev.stopPropagation();
+        document.querySelectorAll(".container-img .card").forEach(el => {
             el.classList.add("hidden");
         }
-    )
-    this.parentNode.classList.add("active");    
-    this.classList.add("active")
-})
-})
-
-document.querySelectorAll(".container-img").forEach(el=>{
-    el.addEventListener("click", function(ev){
-    this.classList.remove("active");
-    this.querySelectorAll(".card").forEach(el=>{
-        el.classList.remove("active");
-        el.classList.remove("hidden");
-
+        )
+        this.parentNode.classList.add("active");
+        this.classList.add("hide")
     })
 })
+
+document.querySelectorAll(".btn-detalles").forEach(el => {
+    el.addEventListener("click", function (ev) {
+        this.classList.remove("active");
+        this.querySelectorAll(".card").forEach(el => {
+            el.classList.remove("active");
+            el.classList.remove("hidden");
+
+        })
+    })
+})
+
+document.querySelectorAll(".container-img").forEach(el => {
+    el.addEventListener("click", function (ev) {
+        this.classList.remove("active");
+        this.querySelectorAll(".card").forEach(el => {
+            el.classList.remove("active");
+            el.classList.remove("hidden");
+
+        })
+    })
 });
+
